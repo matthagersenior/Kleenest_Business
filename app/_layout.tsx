8589eacdc2469c18ebcfa0795ca66e40b2a15295
matthagersenior <matthagersr@gmail.com>
2026-09-01 01:28:33 -0500

    Add governance workspace to Business navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout() {
         <Stack.Screen name="reviews" options={{ title: 'Reviews' }} />
         <Stack.Screen name="engagement" options={{ title: 'Engagement' }} />
         <Stack.Screen name="intelligence" options={{ title: 'Growth Intelligence' }} />
+        <Stack.Screen name="governance" options={{ title: 'Governance & Trust' }} />
       </Stack>
     </BusinessWorkspaceProvider>
   );
